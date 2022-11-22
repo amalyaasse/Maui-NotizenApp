cb15e1211fbd9ceb3477627eb80925b893979d18
amalik <amaliksrtik@gmail.com>
2022-11-22 08:52:05 +0100

    my first maui App in November 2022

---
+++ Notes/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace Notes;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("IndieFlower-Regular.ttf", "IndieFlower-Regular");
+				fonts.AddFont("DancingScript-Regular.ttf", "DancingScript-Regular");
+				fonts.AddFont("Kalam-Regular.ttf", "Kalam-Regular");
+			});
+
+		return builder.Build();
+	}
+}
---
+++ Notes/Views/news.xaml.cs
@@ -0,0 +1,9 @@
+namespace Notes.Views;
+
+public partial class news : ContentView
+{
+	public news()
+	{
+		InitializeComponent();
+	}
+}
